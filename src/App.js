c8c0a22a603bee46edfde219b5c151006a7c55b8
hsb-dev <uudd98@gmail.com>
2024-04-09 01:33:25 +0900

    added component, Responsive navbar(done)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./reset.css";
 import logos from "./logo.svg";
 import downArrow from "./img/down-arrow.svg";
+import menuIcon from "./img/menu-icon.svg";
 import searchIcon from "./img/search.svg";
 import "./App.css";
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Navbar></Navbar>
       <Subnavbar></Subnavbar>
+      <NewsBoard></NewsBoard>
     </div>
   );
 }
@@ -17,11 +19,15 @@ function Navbar() {
   return (
     <div>
       <div className="Navbar">
-        <img src={logos} width="32" height="32" />
+        <img src={logos} width="32" height="32" style={{cursor:"pointer"}} />
       <div className="Navbar-right-area">
-        <div className="Search-area"></div>
+        <div className="Search-area">
+          <img src={searchIcon} className="Search-icon" width="24" height="24" />
+          <input type="text" name="name" className="Search-input"></input>
+        </div>
         <div className="Login">로그인</div>
         <div className="Regist">회원가입</div>
+        <img src={menuIcon} className="Menu-hamburger" width="24" height="24"/>
       </div>
       </div>
     </div>
@@ -40,12 +46,26 @@ function Subnavbar(){
 
         <div className="Sort-area">
           <div className="Sort-area-text">sort by newest</div>
-        <img src={downArrow} width="16" height="16" style={{margin: "auto"}}/>
+        <img src={downArrow} width="16" height="16" className="Sort-area-arrow"/>
         </div>
         
       </div>
     </div>
   );
+}
+
+function NewsBoard() {
+  return(
+    <div>
+    </div>
+  )
+}
+
+function Footer() {
+  return(
+    <div>
+    </div>
+  )
 }
 
 export default App;
