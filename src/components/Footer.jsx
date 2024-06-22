6f3683a9f5e77f73a768da949cc2d1f86993f5cf
Monett <monet9812@gmail.com>
2024-06-22 20:59:04 +0900

    release 1.1.0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,18 @@
-import "../styles/Footer.scss"
-import "../styles/Common.css"
+import "../styles/Footer.scss";
+import "../styles/Common.css";
 
 function Footer() {
-    return (
-      <div className="footer">
-        {/* <div className="footer-content font-s">© 2024 Glampers (glampers20s@gmail.com)</div>
+  return (
+    <div className="footer">
+      {/* <div className="footer-content font-s">© 2024 Glampers (glampers20s@gmail.com)</div>
         <div className="footer-content font-s">made by 박준혁 황수범 </div> */}
-        <div className="footer-content">
-          <div>© 2024 Glampers / glampers20s@gmail.com</div>
-          <div>made by 박준혁 황수범</div>
-        </div>
+      <div className="footer-content">
+        <div>© 2024 Glampers / glampers20s@gmail.com</div>
+        <div>made by 박준혁 황수범</div>
+        <div>v1.1.0</div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default Footer;
